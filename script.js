7cdef49de8f4f222cb9c9d081c75319d646be1a4
thomasrib8 <contact@imagineoproduction.com>
2024-11-26 08:33:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Définez ici le nom d'utilisateur et le mot de passe
+    const validUsername = "admin";
+    const validPassword = "Roue2021*";
+
+    if (username === validUsername && password === validPassword) {
+        // Affiche le contenu principal
+        document.getElementById('login-screen').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    } else {
+        // Affiche un message d'erreur
+        document.getElementById('error-message').style.display = 'block';
+    }
+}
+
 function launchApp(url) {
-    // Ouvre l'URL dans une nouvelle fenêtre ou un nouvel onglet
     window.open(url, '_blank');
 }
